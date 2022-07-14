5cc846e8bb4b5cce890c9f3da4ee99a555dcfebf
Clayton Huff <crhuff@ibm.com>
2022-07-14 10:02:37 -0500

    add provider file

---
+++ provider.tf
@@ -0,0 +1,8 @@
+#####################################################
+# IBM Cloud Satellite -  Azure Example
+# Copyright 2022 IBM
+#####################################################
+
+provider "azurerm" {
+  features {}
+}
